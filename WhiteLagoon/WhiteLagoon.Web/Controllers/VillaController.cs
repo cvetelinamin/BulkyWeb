f8b61218476c73bc52842b89822b998d62d6512b
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2024-03-13 22:58:41 +0200

    Create Villa Controller View for Villa list and Create Villa

---
+++ WhiteLagoon/WhiteLagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        public VillaController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            var villas = this.dbContext.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}

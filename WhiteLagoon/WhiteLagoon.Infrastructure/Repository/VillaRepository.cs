0cf61ddf4ebb1ff2616f2bad5b003fa45d633d76
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2024-03-20 23:04:12 +0200

    Add IVillarepository and VillaRepository

---
+++ WhiteLagoon/WhiteLagoon.Infrastructure/Repository/VillaRepository.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+
+        private readonly ApplicationDbContext dbContext;
+        public VillaRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Add(Villa villa)
+        {
+            this.dbContext.Add(villa);
+        }
+
+        public Villa Get(Expression<Func<Villa, bool>> filter, string? includeProperties)
+        {
+            IQueryable<Villa> query = this.dbContext.Set<Villa>();
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (!string.IsNullOrEmpty(includeProperties)
+            {
+                foreach (var property in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(property);
+                }
+            }
+
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
+        {
+            IQueryable<Villa> query = this.dbContext.Set<Villa>();
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if(!string.IsNullOrEmpty(includeProperties)
+            {
+                foreach(var property in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(property);
+                }
+            }
+
+            return query.ToList();
+        }
+
+        public void Remove(Villa villa)
+        {
+            this.dbContext.Remove(villa);
+        }
+
+        public void Save()
+        {
+            this.dbContext.SaveChanges();
+        }
+
+        public void Update(Villa villa)
+        {
+            this.dbContext.Update(villa);
+        }
+    }
+}

e2256eec1dca599570a32928a43feef186221b8d
Tsvetelina Georgieva <cvetelina.min.georgieva@gmail.com>
2024-04-07 23:03:41 +0300

    Add Login and Register UI

---
+++ WhiteLagoon/WhiteLagoon.Web/Controllers/AccountController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Repository;
+using WhiteLagoon.Web.ViewModels;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AccountController(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.unitOfWork = unitOfWork;
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.roleManager = roleManager;
+        }
+        public IActionResult Login(string returnUrl = null)
+        {
+            returnUrl ??= Url.Content("~/");
+            LoginVM loginVM = new ()
+            {
+                RedirectUrl = returnUrl,
+            };
+            return View(loginVM);
+        }
+
+        public IActionResult Register()
+        {
+            if(!this.roleManager.RoleExistsAsync("Admin").GetAwaiter().GetResult())
+            {
+                this.roleManager.CreateAsync(new IdentityRole("Admin")).Wait();
+                this.roleManager.CreateAsync(new IdentityRole("Customer")).Wait();
+            }
+            RegisterVM registerVM = new ()
+            {
+                RoleList = this.roleManager.Roles.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                })
+            };
+
+            return View(registerVM);
+        }
+    }
+}
